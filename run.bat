260f9f6e8730a84e860f326f45866387abfd8c8c
Nicky <34005574+lc700x@users.noreply.github.com>
2025-07-28 23:28:13 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,15 @@
 @echo off
+echo Running Desktop2Stereo...
+
+:: Set paths
 Set "VIRTUAL_ENV=.env"
+
+:: Check if environment exists
 If Not Exist "%VIRTUAL_ENV%\Scripts\activate.bat" Exit /B 1
-echo - Virtual enviroment activation
+
+echo - Virtual environment activation
 Call "%VIRTUAL_ENV%\Scripts\activate.bat"
+
 python main.py --hf-mirror
-@REM --hf-mirror is used to set the Hugging Face mirror for the user cannot access HuggingFace directly.
+
+@REM --hf-mirror is used to set the Hugging Face mirror for users who cannot access HuggingFace directly.
