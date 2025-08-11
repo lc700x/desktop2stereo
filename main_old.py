8860c471d459d43be28fec9cfe8b079ecb3cfe38
lc700x <zjulianxu@gmail.com>
2025-08-11 15:43:27 +0800

    Correct old main

--- main_old.py
+++ main_old.py
@@ -83,7 +83,6 @@ def main():
         try:
             # Get latest frame, or skip update
             frame_rgb, depth = depth_q.get_nowait()
-            depth = depth.cpu().numpy().astype('float32')
             window.update_frame(frame_rgb, depth)
         except queue.Empty:
             time.sleep(TIME_SLEEP)  # Reuse previous frame if none available
