153cddc59adbcdef898a1ad8343500c0b9be261f
Nicky <34005574+lc700x@users.noreply.github.com>
2025-08-12 07:08:56 +0800

    Update depth.py

--- depth.py
+++ depth.py
@@ -282,3 +282,4 @@ def make_sbs_tensor(rgb, depth, ipd_uv=0.03, depth_strength=1.0, half=False):
         return sbs_half.clamp(0, 255).byte().cpu().numpy()  # (H, W, 3)
 
     return sbs_full.clamp(0, 255).byte().cpu().numpy()  # (H, 2W, 3)
+
