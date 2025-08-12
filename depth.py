Correct FPS calculation

depth.py
@@ -48,13 +48,12 @@ def get_device(index=0):
             dev = torch_directml.device(index)
             info = f"Using DirectML device: {torch_directml.device_name(index)}"
             return dev, info
-    except ImportError:
+        if torch.cuda.is_available():
+            return torch.device("cuda"), f"Using CUDA device: {torch.cuda.get_device_name(index)}"
+        if torch.backends.mps.is_available():
+            return torch.device("mps"), "Using Apple Silicon (MPS) device"
+    except:
         pass
-
-    if torch.cuda.is_available():
-        return torch.device("cuda"), f"Using CUDA device: {torch.cuda.get_device_name(index)}"
-    if torch.backends.mps.is_available():
-        return torch.device("mps"), "Using Apple Silicon (MPS) device"
     return torch.device("cpu"), "Using CPU device"
 
 