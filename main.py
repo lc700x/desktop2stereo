c3e49c21674160df94b64694c9264408cc11379a
lc700x <zjulianxu@gmail.com>
2025-08-13 03:06:44 +0800

    Update Main.py

--- main.py
+++ main.py
@@ -86,14 +86,13 @@ def main():
             depth = depth.detach().cpu().numpy().astype('float32')
             window.update_frame(frame_rgb, depth)
         except queue.Empty:
-            pass  # Reuse previous frame if none available
+            time.sleep(TIME_SLEEP)  # Reuse previous frame if none available
 
         window.render()
         glfw.swap_buffers(window.window)
-        glfw.wait_events_timeout(TIME_SLEEP)
+        glfw.poll_events()
 
     glfw.terminate()
 
 if __name__ == "__main__":
     main()
-
