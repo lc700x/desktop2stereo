f3e67786b54790011e22458eae00d9baf28f4e92
Nicky <34005574+lc700x@users.noreply.github.com>
2025-08-12 21:03:56 +0800

    Add standalone scripts for python

---
+++ install-cuda_standalone.bat
@@ -0,0 +1,51 @@
+@echo off
+echo --- Desktop2Stereo Installer (With CUDA for NVIDIA GPUs.) ---
+echo - Setting up the virtual environment
+
+@REM Set paths
+Set "VIRTUAL_ENV=.env"
+Set "PYTHON_EXE=.\Python310\python.exe"
+
+@REM Create virtual environment if it doesn't exist
+if not exist "%VIRTUAL_ENV%\Scripts\activate.bat" (
+    echo Creating virtual environment...
+    %PYTHON_EXE% -m venv "%VIRTUAL_ENV%"
+    if %errorlevel% neq 0 (
+        echo Failed to create virtual environment
+        pause
+        exit /b 1
+    )
+)
+
+@REM Activate virtual environment
+echo - Virtual environment activation
+call "%VIRTUAL_ENV%\Scripts\activate.bat"
+if %errorlevel% neq 0 (
+    echo Failed to activate virtual environment
+    pause
+    exit /b 1
+)
+
+@REM Update pip
+echo - Updating the pip package
+python -m pip install --upgrade pip --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
+if %errorlevel% neq 0 (
+    echo Failed to update pip
+    pause
+    exit /b 1
+)
+
+@REM Install requirements
+echo.
+echo - Installing the requirements
+python -m pip install -r requirements-cuda.txt --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
+python -m pip install -r requirements.txt --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
+python -m pip install wincam==1.0.14 --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
+if %errorlevel% neq 0 (
+    echo Failed to install requirements
+    pause
+    exit /b 1
+)
+
+echo Python environment deployed successfully.
+pause
