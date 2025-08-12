a1aeb338e37398cddf62691b4bd5f51a840dbdbd
lc700x <zjulianxu@gmail.com>
2025-08-13 01:32:10 +0800

    Correct FPS calculation

--- main.py
+++ main.py
@@ -82,14 +82,14 @@ def main():
                 glfw.set_window_title(window.window, f"Stereo Viewer | depth: {window.depth_ratio:.1f} | FPS: {fps:.1f}")
         try:
             # Get latest frame, or skip update
-            frame_rgb, depth = depth_q.get_nowait()
+            frame_rgb, depth = depth_q.get(timeout=TIME_SLEEP)
             window.update_frame(frame_rgb, depth)
         except queue.Empty:
-            time.sleep(TIME_SLEEP)  # Reuse previous frame if none available
+            pass  # Reuse previous frame if none available
 
         window.render()
         glfw.swap_buffers(window.window)
-        glfw.poll_events()
+        glfw.wait_events_timeout(TIME_SLEEP)
 
     glfw.terminate()
 
