60f5ecbbdab340bf702d3557cdee7cac61fc7932
Nicky <34005574+lc700x@users.noreply.github.com>
2025-08-18 16:54:05 +0800

    Create convert.py for convert PyTorch model to HF

---
+++ convert.py
@@ -0,0 +1,35 @@
+import argparse
+import os
+import torch
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path',
+                        help="model and tokenizer path",
+                        default='./',
+                        )
+    return parser.parse_args()
+
+
+def convert_from_pretrained(model_path):
+    model_path = model_path.replace('& ', '').replace("\\", '/').replace("'", '') # Convert Windows Path
+    from transformers import AutoModelForDepthEstimation
+    model = AutoModelForDepthEstimation.from_pretrained(
+        pretrained_model_name_or_path=model_path,
+        low_cpu_mem_usage=True,
+        trust_remote_code=True,
+        torch_dtype=torch.float32)  #float32 for best compatibility
+    # Convert to .safetensors format
+    model.save_pretrained(model_path, safe_serialization=True)
+
+
+if __name__ == '__main__':
+    # Usage:
+    # 1. Put model (pytorch_model.bin, model.safetensors, tf_model.h5, model.ckpt.index or flax_model.msgpack) into a folder ModelName/
+    # 2. Run the following:
+    # python convert.py --model_path path_to_ModelName
+
+    args = parse_arguments()
+
+    print(f"covert  {args.model_path} into HuggingFace safetensor")
+    convert_from_pretrained(args.model_path)
