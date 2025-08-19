befc8b588c3283f41cff516a01f4683280f7ea44
Nicky <34005574+lc700x@users.noreply.github.com>
2025-08-19 15:50:03 +0800

    Update convert

--- convert_depth_anything_hf/convert_dpt.py
+++ convert_depth_anything_hf/convert_dpt.py
@@ -4,13 +4,13 @@
 import torch
 from transformers import Dinov2Model, DepthAnythingConfig, DepthAnythingForDepthEstimation
 import os
+
 DEVICE = "cpu"
 MODEL_ID = "C:/Users/xul/Downloads/video_depth_anything_large"
 MODEL_PATH = MODEL_ID + "/video_depth_anything_vitl.pth"
 DTYPE = torch.float16
 BACK_BONE = "facebook/dinov2-small"
-OUTPUT_ID = "depth-anything-video-large-hf"
-
+OUTPUT_ID = "depth_anything_video_large_hf" # cannot use "-"
 # Load DINOv2 backbone
 dinov2 = Dinov2Model.from_pretrained(BACK_BONE)
 
@@ -28,7 +28,7 @@ model = DepthAnythingForDepthEstimation(config)
 model.backbone = dinov2
 
 # Load the rest of the checkpoint (decoder, etc.)
-checkpoint = torch.load(MODEL_PATH, map_location="cpu")
+checkpoint = torch.load(MODEL_PATH, map_location=DEVICE)
 missing_keys, unexpected_keys = model.load_state_dict(checkpoint, strict=False)
 
 # print("Missing keys:", missing_keys)
