73b0d99c013e92605cd2aa6b3c75ed4d2cdeacdb
Nicky <34005574+lc700x@users.noreply.github.com>
2025-08-19 15:41:24 +0800

    Fix weight loss during convertion

---
+++ convert_depth_anything_hf/convert_dpt.py
@@ -0,0 +1,39 @@
+
+
+
+import torch
+from transformers import Dinov2Model, DepthAnythingConfig, DepthAnythingForDepthEstimation
+import os
+DEVICE = "cpu"
+MODEL_ID = "C:/Users/xul/Downloads/video_depth_anything_large"
+MODEL_PATH = MODEL_ID + "/video_depth_anything_vitl.pth"
+DTYPE = torch.float16
+BACK_BONE = "facebook/dinov2-small"
+OUTPUT_ID = "depth-anything-video-large-hf"
+
+# Load DINOv2 backbone
+dinov2 = Dinov2Model.from_pretrained(BACK_BONE)
+
+# Create Depth Anything config
+config = DepthAnythingConfig(
+    backbone=BACK_BONE,
+    use_pretrained_backbone=False,
+    depth_estimation_type="relative"
+)
+
+# Initialize Depth Anything model
+model = DepthAnythingForDepthEstimation(config)
+
+# Inject the pretrained DINOv2 backbone
+model.backbone = dinov2
+
+# Load the rest of the checkpoint (decoder, etc.)
+checkpoint = torch.load(MODEL_PATH, map_location="cpu")
+missing_keys, unexpected_keys = model.load_state_dict(checkpoint, strict=False)
+
+# print("Missing keys:", missing_keys)
+# print("Unexpected keys:", unexpected_keys)
+
+# Save the converted model
+model.save_pretrained(OUTPUT_ID)
+print(f"✅ Model saved to {OUTPUT_ID}")
