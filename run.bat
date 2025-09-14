9aebd89a23f326c490057da6f2cd4ffcb71344bf
lc700x <zjulianxu@gmail.com>
2025-09-14 22:43:12 +0800

    Update file list for updator only

--- run.bat
+++
@@ -1,4 +0,0 @@
-call .env\Scripts\activate
-start "" python -m gui
-@REM start "" pythonw -m gui
-exit /b 0
--- run_mac
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-source ".env/bin/activate"
-python3 gui.py
