061e4bbd177515e481b877e33665f607f03e5c05
lc700x <zjulianxu@gmail.com>
2025-09-23 03:45:02 +0800

    Finalize GUI and Python Pytorch version for 2.3.4

--- install-cuda_standalone.bat
+++ install-cuda_standalone.bat
@@ -3,32 +3,12 @@ echo --- Desktop2Stereo Installer (With CUDA for NVIDIA GPUs.) ---
 echo - Setting up the virtual environment
 
 @REM Set paths
-Set "VIRTUAL_ENV=.env"
 Set "PYTHON_EXE=.\Python311\python.exe"
 
-@REM Create virtual environment if it doesn't exist
-if not exist "%VIRTUAL_ENV%\Scripts\activate.bat" (
-    echo Creating virtual environment...
-    %PYTHON_EXE% -m venv "%VIRTUAL_ENV%"
-    if %errorlevel% neq 0 (
-        echo Failed to create virtual environment
-        pause
-        exit /b 1
-    )
-)
-
-@REM Activate virtual environment
-echo - Virtual environment activation
-call "%VIRTUAL_ENV%\Scripts\activate.bat"
-if %errorlevel% neq 0 (
-    echo Failed to activate virtual environment
-    pause
-    exit /b 1
-)
 
 @REM Update pip
 echo - Updating the pip package
-$PYTHON_EXE -m pip install --upgrade pip --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install --upgrade pip --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
 if %errorlevel% neq 0 (
     echo Failed to update pip
     pause
@@ -38,12 +18,12 @@ if %errorlevel% neq 0 (
 @REM Install requirements
 echo.
 echo - Installing the requirements
-$PYTHON_EXE -m pip install -r requirements-cuda.txt --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
-$PYTHON_EXE -m pip install tensorrt-cu12==10.13.3.9 --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
-$PYTHON_EXE -m pip install "triton<3.5" --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
-$PYTHON_EXE -m pip install onnx==1.19.0 --no-cache-dir --trusted-host  http://mirrors.aliyun.com/pypi/simple/
-$PYTHON_EXE -m pip install -r requirements.txt --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
-$PYTHON_EXE -m pip install wincam==1.0.14 --no-cache-dir --trusted-host  http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install -r requirements-cuda.txt --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install tensorrt==10.13.3.9 --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install "triton-windows<3.4" --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install onnx==1.19.0 --no-cache-dir --no-warn-script-location --trusted-host  http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install -r requirements.txt --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install wincam==1.0.14 --no-cache-dir --no-warn-script-location --trusted-host  http://mirrors.aliyun.com/pypi/simple/
 if %errorlevel% neq 0 (
     echo Failed to install requirements
     pause
--- install-dml_standalone.bat
+++ install-dml_standalone.bat
@@ -1,46 +1,25 @@
 @echo off
-echo --- Desktop2Stereo Installer  (With Windows DirectML for AMD GPUs and etc.) ---
+echo --- Desktop2Stereo  Installer (With Windows DirectML for AMD GPUs and etc.) ---
 echo - Setting up the virtual environment
 
 @REM Set paths
-Set "VIRTUAL_ENV=.env"
 Set "PYTHON_EXE=.\Python311\python.exe"
-
-@REM Create virtual environment if it doesn't exist
-if not exist "%VIRTUAL_ENV%\Scripts\activate.bat" (
-    echo Creating virtual environment...
-    %PYTHON_EXE% -m venv "%VIRTUAL_ENV%"
-    if %errorlevel% neq 0 (
-        echo Failed to create virtual environment
-        pause
-        exit /b 1
-    )
-)
-
-@REM Activate virtual environment
-echo - Virtual environment activation
-call "%VIRTUAL_ENV%\Scripts\activate.bat"
-if %errorlevel% neq 0 (
-    echo Failed to activate virtual environment
-    pause
-    exit /b 1
-)
 
 @REM Update pip
 echo - Updating the pip package
-%PYTHON_EXE% -m pip install --upgrade pip --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install --upgrade pip --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
 if %errorlevel% neq 0 (
     echo Failed to update pip
     pause
     exit /b 1
 )
 
-@REM Install requirements
+@REM --no-cache-dir --no-warn-script-location --no-warn-script-location requirements
 echo.
-echo - Installing the requirements
-%PYTHON_EXE% -m pip install -r requirements-dml.txt --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
-%PYTHON_EXE% -m pip install -r requirements.txt --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
-%PYTHON_EXE% -m pip install wincam==1.0.14 --no-cache-dir --trusted-host http://mirrors.aliyun.com/pypi/simple/
+echo - --no-cache-dir --no-warn-script-location --no-warn-script-locationing the requirements
+%PYTHON_EXE% -m pip install -r requirements-dml.txt --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install -r requirements.txt --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install wincam==1.0.14 --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
 if %errorlevel% neq 0 (
     echo Failed to install requirements
     pause
