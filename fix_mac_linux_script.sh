6758fd31c7067ad67af2a86506581eec01aeff32
lc700x <zjulianxu@gmail.com>
2025-11-05 23:38:10 +0800

    udapte fix script

--- fix_mac_linux_script.sh
+++ fix_mac_linux_script.sh
@@ -1,8 +1,27 @@
-sed -i '' -e 's/\r$//' install-mps
-sed -i '' -e 's/\r$//' run_mac
-sed -i '' -e 's/\r$//' update_mac_linux
+#!/bin/bash
 
-sed -i '' -e 's/\r$//' run_linux.bash
-sed -i '' -e 's/\r$//' install-cuda.bash
-sed -i '' -e 's/\r$//' install-rocm7.bash
-sed -i '' -e 's/\r$//' install-rocm.bash
+# List of files to clean
+FILES=(
+  install-mps
+  run_mac
+  update_mac_linux
+  run_linux.bash
+  install-cuda.bash
+  install-rocm7.bash
+  install-rocm.bash
+  install-cuda0.bash
+)
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  for file in "${FILES[@]}"; do
+    if [[ -f "$file" ]]; then
+      sed -i '' -e 's/\r$//' "$file"
+    fi
+  done
+else
+  for file in "${FILES[@]}"; do
+    if [[ -f "$file" ]]; then
+      sed -i -e 's/\r$//' "$file"
+    fi
+  done
+fi
