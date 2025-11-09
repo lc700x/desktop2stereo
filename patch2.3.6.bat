1adf83847b5ab6d49112409e964dc9a98fe4ddaf
Nicky <34005574+lc700x@users.noreply.github.com>
2025-11-09 14:09:55 +0800

    Add files via upload

---
+++ patch2.3.6.bat
@@ -0,0 +1,27 @@
+@echo off
+@REM Set paths
+Set "PYTHON_EXE=.\Python311\python.exe"
+
+
+@REM Update pip
+echo [Desktop2Stereo Patch 2.3.5]
+echo - Updating the pip package
+%PYTHON_EXE% -m pip install --upgrade pip --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
+if %errorlevel% neq 0 (
+    echo Failed to update pip
+    pause
+    exit /b 1
+)
+
+@REM Install new requirements
+echo.
+echo - Installing new requirements
+%PYTHON_EXE% -m pip install -r requirements.txt --no-cache-dir --no-warn-script-location --trusted-host  http://mirrors.aliyun.com/pypi/simple/
+if %errorlevel% neq 0 (
+    echo Failed to install requirements
+    pause
+    exit /b 1
+)
+
+echo Python environment updated successfully.
+pause
