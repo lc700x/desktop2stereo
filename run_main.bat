cf00735319083d60b4a70b9e5d8e4fd1afd9b46a
lc700x <zjulianxu@gmail.com>
2025-11-26 19:34:42 +0800

    Add main for WIndows system python version

--- run_main.bat
+++ run_main.bat
@@ -1,5 +1,4 @@
-@echo off
-Set "PYTHON_EXE=.\Python311\python.exe"
-%PYTHON_EXE% -m main
-@REM start "" pythonw -m main
+call .env\Scripts\activate
+start "" python -m main
+@REM start "" pythonw -m gui
 exit /b 0
---
+++ run_windows_main.bat
@@ -0,0 +1,5 @@
+@echo off
+Set "PYTHON_EXE=.\Python311\python.exe"
+%PYTHON_EXE% -m main
+@REM start "" pythonw -m main
+exit /b 0
