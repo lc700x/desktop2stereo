7c99dde500a5e10c382d18fb25857204edb80da4
lc700x <zjulianxu@gmail.com>
2025-12-22 02:03:49 +0800

    correct main

--- run_windows_main.bat
+++ run_windows_main.bat
@@ -1,6 +1,6 @@
 @echo off
 set PYTHONPATH=.\Python311\python.exe:$PYTHONPATH
 Set "PYTHON_EXE=.\Python311\python.exe"
-%PYTHON_EXE% -m gui
+%PYTHON_EXE% -m main
 @REM start "" pythonw -m gui
 exit /b 0
